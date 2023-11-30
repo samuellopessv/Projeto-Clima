376d45dd90b53be0a5cfb93205d6d389d6dedf05
samuel lopes <ldavi3002@gmail.com>
2023-11-30 20:57:20 -0300

    atualizei

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+//5d8e068e9ac014e94ba084d5bc08d23c
+
+
+
+https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=5d8e068e9ac014e94ba084d5bc08d23c
+https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={5d8e068e9ac014e94ba084d5bc08d23c}
