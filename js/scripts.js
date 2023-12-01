4bd9409997fa99c957c8dca6fff1365eeb98d066
samuel lopes <ldavi3002@gmail.com>
2023-12-01 07:39:34 -0300

    atualizei5

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,27 @@ const apiCountryURL = "https://flagsapi.com/"
 const cityInput = document.querySelector('#city-input')
 const searchBtn = document.querySelector('#search')
 
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const umidityElement = document.querySelector("#umidity span")
+const windElement = document.querySelector("#wind span")
+
 //Funções 
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKEy}&lang=pt_br`
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    console.log(data)
+
+}
+
 const showWeatherData = (city) => {
-    console.log(city)
+    apiWeatherURL(city)
 }
 
 
