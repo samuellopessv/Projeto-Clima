998c8cbd8c8492a2f32c3c30baed336953793e07
samuel lopes <ldavi3002@gmail.com>
2023-12-01 07:17:11 -0300

    atualizei.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
-//5d8e068e9ac014e94ba084d5bc08d23c
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=5d8e068e9ac014e94ba084d5bc08d23c
+
+
+//Variaveis e selecão de lementos
+const apiKEy= "5d8e068e9ac014e94ba084d5bc08d23c"
+const apiCountryURL = "https://flagsapi.com/"
+
+const cityInput = document.querySelector('#city-input')
+const searchBtn = document.querySelector('#search')
+
+//Funções 
 
 
 
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=5d8e068e9ac014e94ba084d5bc08d23c
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={5d8e068e9ac014e94ba084d5bc08d23c}
+//Eventos
+searchBtn.addEventListener('click', (e) =>{
+
+    e.preventDefault()
+
+    console.log("teste")
+})
+
